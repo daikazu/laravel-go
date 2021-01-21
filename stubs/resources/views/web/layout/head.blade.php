5760244be8320df1d9b87ee8b0f894035fe1beb7
Mike Wall <daikazu@gmail.com>
2021-01-20 19:22:51 -0500

    removed duplicate html tag

--- stubs/resources/views/web/layout/head.blade.php
+++ stubs/resources/views/web/layout/head.blade.php
@@ -1,5 +1,4 @@
 @include('web.layout.head.tracking')
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
 <meta name="csrf-token" content="{{ csrf_token() }}">
