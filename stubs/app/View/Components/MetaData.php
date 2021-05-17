0b67765b451b8766cefe06398b818e2949579526
Mike Wall <daikazu@gmail.com>
2021-05-17 15:40:54 -0400

    Update MetaData.php

--- stubs/app/View/Components/MetaData.php
+++ stubs/app/View/Components/MetaData.php
@@ -5,8 +5,6 @@ namespace App\View\Components;
 use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\SEOTools;
 use Illuminate\View\Component;
-use Symfony\Component\Yaml\Exception\ParseException;
-use Symfony\Component\Yaml\Yaml;
 
 class MetaData extends Component
 {
@@ -19,11 +17,6 @@ class MetaData extends Component
      */
     public function __construct()
     {
-        try {
-            $this->meta = collect(Yaml::parse(app()->view->getSection('meta')));
-        } catch (ParseException $exception) {
-            // printf('Unable to parse the YAML string: %s', $exception->getMessage());
-        }
     }
 
     /**
@@ -39,15 +32,9 @@ class MetaData extends Component
         if (app()->view->getSection('meta_description')) {
             SEOTools::setDescription(app()->view->getSection('meta_description'));
         }
-
-        SEOTools::opengraph()->setUrl(app('url')->full());
-
         if (app()->view->getSection('meta_canonical')) {
             SEOMeta::setCanonical(app()->view->getSection('meta_canonical'));
-        } else {
-            SEOTools::setCanonical(app('url')->full());
         }
-
         return view('web.layout.head.meta-data');
     }
 }
