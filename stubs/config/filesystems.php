28f89941b592fd79431cb56b1edff9fd5bab1768
Mike Wall <daikazu@gmail.com>
2021-05-17 11:15:57 -0400

    Update filesystems.php

--- stubs/config/filesystems.php
+++ stubs/config/filesystems.php
@@ -53,8 +53,13 @@ return [
         ],
 
         'backup' => [
-            'driver' => 'local',
-            'root'   => storage_path('app'),
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BACKUP_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
         ]
 
     ],
