3e2142f0395ebf8bdfbcce5aecbb1959b592965d
Mike Wall <daikazu@gmail.com>
2022-10-31 09:12:28 -0400

    update

--- stubs/base/vite.config.js
+++ stubs/base/vite.config.js
@@ -1,48 +1,33 @@
 import {defineConfig} from 'vite';
 import laravel from 'laravel-vite-plugin';
 import livewire from '@defstudio/vite-livewire-plugin';
-import fs from "fs";
 import {homedir} from "os";
 import {resolve} from "path";
 
+let host = "example.test";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/Http/Livewire/**',
+                // 'app/Forms/Components/**',
+            ],
         }),
         livewire({
             refresh: ['resources/css/app.css'],
         })
     ],
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: resolve(homedir(), `.config/valet/Certificates/${host}.key`),
+            cert: resolve(homedir(), `.config/valet/Certificates/${host}.crt`),
+        },
+    },
 });
 
-/**
- * Making Vite and Valet play nice together
- * https://freek.dev/2276-making-vite-and-valet-play-nice-together
- *
- * comment this out if you don't use Valet
- */
 
-function detectServerConfig(host) {
-    let keyPath = resolve(homedir(), `.config/valet/Certificates/${host}.key`);
-    let certificatePath = resolve(homedir(), `.config/valet/Certificates/${host}.crt`);
-
-    if (!fs.existsSync(keyPath)) {
-        return {};
-    }
-
-    if (!fs.existsSync(certificatePath)) {
-        return {};
-    }
-
-    return {
-        hmr: {host},
-        host,
-        https: {
-            key: fs.readFileSync(keyPath),
-            cert: fs.readFileSync(certificatePath),
-        },
-    };
-}
